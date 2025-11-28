399fcb4e390cb597cfe02e54215db1f96e684219
Bashir Rased <bashir.rased@gmail.com>
2025-11-28 11:50:16 +0600

    New version

--- rb-site-social-links.php
+++ rb-site-social-links.php
@@ -2,413 +2,231 @@
 /**
  * Plugin Name:       RB Site Social Links
  * Plugin URI:        https://github.com/BashirRased/wp-plugin-rb-site-social-links
- * Description:       RB Site Social Links plugin use for your site social links add.
- * Version:           1.0.6
+ * Description:       RB Site Social Links plugin allows you to add and manage your site's social links.
+ * Version:           1.0.7
  * Requires at least: 6.4
- * Tested up to: 6.8
- * Requires PHP: 7.0
+ * Tested up to:      6.7
+ * Requires PHP:      7.4
+ * PHP Version:       8.2
  * Author:            Bashir Rased
- * Author URI:        https://profiles.wordpress.org/bashirrased2017/
+ * Author URI:        https://bashir-rased.com/
  * Text Domain:       rb-site-social-links
- * Domain Path: 	  /languages
+ * Domain Path:       /languages
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package RB_Plugins
+ * @subpackage RB_Site_Social_Links
  */
 
-// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Plugin Text domain loaded
-function rb_plugin_textdomain() {
-    load_plugin_textdomain('rb-site-social-links', false, dirname(plugin_basename(__FILE__)).'/languages'); 
-}
-add_action('plugins_loaded', 'rb_plugin_textdomain');
-
-// Settings Page Link
-add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
-	$link = sprintf("<a href='%s' style='color:#b32d2e;'>%s</a>", admin_url('options-general.php#rb-social-links-section'), __('Settings', 'rb-site-social-links'));
-	array_push($links, $link);
-	return $links;
-});
-
-// Github Page Link
-add_filter('plugin_row_meta', function ($links, $plugin) {
-	if (plugin_basename(__FILE__) == $plugin) {
-		$link = sprintf("<a href='%s' style='color:#b32d2e;'>%s</a>", esc_url('https://github.com/BashirRased/wp-plugin-rb-site-social-links'), __('Fork on Github', 'rb-site-social-links'));
-		array_push($links, $link);
+if ( ! function_exists( 'rb_plugin_textdomain' ) ) {
+	/**
+	 * Load plugin text domain.
+	 *
+	 * @return void
+	 */
+	function rb_plugin_textdomain() {
+		load_plugin_textdomain( 'rb-site-social-links', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 	}
-	return $links;
-}, 10, 2);
+}
+add_action( 'plugins_loaded', 'rb_plugin_textdomain' );
 
-function rb_site_social_links() {
-    add_settings_section(
-        'rb_site_social_links_section',
-        __('Socail Media Links', 'rb-site-social-links'),
-        'rb_site_social_links_section_desc',
-        'general'
-    );
-    
-    // Facebook Link Register Here
-    add_settings_field(
-        'rb_facebook_link',
-        __('Facebook Link:', 'rb-site-social-links'),
-        'rb_facebook_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_facebook_link"]
-    );
-    register_setting(
-        'general',
-        'rb_facebook_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
+add_filter(
+	'plugin_action_links_' . plugin_basename( __FILE__ ),
+	function ( $links ) {
+		$link    = sprintf(
+			'<a href="%s" style="color:#b32d2e;">%s</a>',
+			esc_url( admin_url( 'options-general.php#rb-social-links-section' ) ),
+			esc_html__( 'Settings', 'rb-site-social-links' )
+		);
+		$links[] = $link;
+		return $links;
+	}
+);
 
-    // Twitter Link Register Here
-    add_settings_field(
-        'rb_twitter_link',
-        __('Twitter Link:', 'rb-site-social-links'),
-        'rb_twitter_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_twitter_link"]
-    );
-    register_setting(
-        'general',
-        'rb_twitter_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
+add_filter(
+	'plugin_row_meta',
+	function ( $links, $plugin ) {
+		if ( plugin_basename( __FILE__ ) === $plugin ) {
+			$link    = sprintf(
+				'<a href="%s" style="color:#b32d2e;">%s</a>',
+				esc_url( 'https://github.com/BashirRased/wp-plugin-rb-site-social-links' ),
+				esc_html__( 'Fork on GitHub', 'rb-site-social-links' )
+			);
+			$links[] = $link;
+		}
+		return $links;
+	},
+	10,
+	2
+);
 
-    // Instagram Link Register Here
-    add_settings_field(
-        'rb_instagram_link',
-        __('Instagram Link:', 'rb-site-social-links'),
-        'rb_instagram_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_instagram_link"]
-    );
-    register_setting(
-        'general',
-        'rb_instagram_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
+if ( ! function_exists( 'rb_site_social_links' ) ) {
+	/**
+	 * Register social links settings fields.
+	 *
+	 * @return void
+	 */
+	function rb_site_social_links() {
+		add_settings_section(
+			'rb_site_social_links_section',
+			esc_html__( 'Social Media Links', 'rb-site-social-links' ),
+			'rb_site_social_links_section_desc',
+			'general'
+		);
 
-    // LinkedIn Link Register Here
-    add_settings_field(
-        'rb_linkedin_link',
-        __('LinkedIn Link:', 'rb-site-social-links'),
-        'rb_linkedin_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_linkedin_link"]
-    );
-    register_setting(
-        'general',
-        'rb_linkedin_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
+		$social_links = array(
+			'rb_facebook_link'  => array( 'Facebook Link:', 'https://www.facebook.com/bashir.rased' ),
+			'rb_twitter_link'   => array( 'Twitter Link:', 'https://twitter.com/' ),
+			'rb_instagram_link' => array( 'Instagram Link:', 'https://www.instagram.com/md.rashedmollik/' ),
+			'rb_linkedin_link'  => array( 'LinkedIn Link:', 'https://www.linkedin.com/in/bashir-rased/' ),
+			'rb_pinterest_link' => array( 'Pinterest Link:', 'https://www.pinterest.com/bashirrased2/' ),
+			'rb_youtube_link'   => array( 'YouTube Link:', 'www.youtube.com/@bashirrased4926' ),
+			'rb_tiktok_link'    => array( 'TikTok Link:', 'https://www.tiktok.com/@bashirrased' ),
+			'rb_dribbble_link'  => array( 'Dribbble Link:', 'https://www.dribbble.com/bashirrased2/' ),
+			'rb_behance_link'   => array( 'Behance Link:', 'https://www.behance.net/bashirrased2021' ),
+			'rb_whatsapp_link'  => array( 'WhatsApp Link:', 'https://wa.me/8801934109870' ),
+			'rb_github_link'    => array( 'GitHub Link:', 'https://github.com/BashirRased' ),
+		);
 
-    // Pinterest Link Register Here
-    add_settings_field(
-        'rb_pinterest_link',
-        __('Pinterest Link:', 'rb-site-social-links'),
-        'rb_pinterest_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_pinterest_link"]
-    );
-    register_setting(
-        'general',
-        'rb_pinterest_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
+		foreach ( $social_links as $id => $data ) {
+			add_settings_field(
+				$id,
+				esc_html( $data[0] ),
+				'rb_site_social_links_input',
+				'general',
+				'rb_site_social_links_section',
+				array(
+					'label_for'   => $id,
+					'placeholder' => $data[1],
+				)
+			);
 
-    // YouTube Link Register Here
-    add_settings_field(
-        'rb_youtube_link',
-        __('YouTube Link:', 'rb-site-social-links'),
-        'rb_youtube_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_youtube_link"]
-    );
-    register_setting(
-        'general',
-        'rb_youtube_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
+			register_setting(
+				'general',
+				$id,
+				array(
+					'sanitize_callback' => 'esc_url_raw',
+				)
+			);
+		}
+	}
+}
+add_action( 'admin_init', 'rb_site_social_links' );
 
-    // Behance Link Register Here
-    add_settings_field(
-        'rb_behance_link',
-        __('Behance Link:', 'rb-site-social-links'),
-        'rb_behance_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_behance_link"]
-    );
-    register_setting(
-        'general',
-        'rb_behance_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
-
-    // Dribbble Link Register Here
-    add_settings_field(
-        'rb_dribbble_link',
-        __('Dribbble Link:', 'rb-site-social-links'),
-        'rb_dribbble_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_dribbble_link"]
-    );
-    register_setting(
-        'general',
-        'rb_dribbble_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
-
-    // Github Link Register Here
-    add_settings_field(
-        'rb_github_link',
-        __('Github Link:', 'rb-site-social-links'),
-        'rb_github_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_github_link"]
-    );
-    register_setting(
-        'general',
-        'rb_github_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
-
-    // CodePen Link Register Here
-    add_settings_field(
-        'rb_codepen_link',
-        __('CodePen Link:', 'rb-site-social-links'),
-        'rb_codepen_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_codepen_link"]
-    );
-    register_setting(
-        'general',
-        'rb_codepen_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
-
-    // WordPress Link Register Here
-    add_settings_field(
-        'rb_wordpress_link',
-        __('WordPress Link:', 'rb-site-social-links'),
-        'rb_wordpress_link_input',
-        'general',
-        'rb_site_social_links_section',
-        ["label_for" => "rb_wordpress_link"]
-    );
-    register_setting(
-        'general',
-        'rb_wordpress_link',
-        array(
-            'sanitize_callback' => 'esc_attr',
-        )
-    );
-    
+if ( ! function_exists( 'rb_site_social_links_section_desc' ) ) {
+	/**
+	 * Section description callback.
+	 *
+	 * @return void
+	 */
+	function rb_site_social_links_section_desc() {
+		printf(
+			'<p id="rb-social-links-section">%s</p>',
+			esc_html__( 'Add your website social media links.', 'rb-site-social-links' )
+		);
+	}
 }
 
-function rb_site_social_links_section_desc() {
-	printf(
-		'<p id="rb-social-links-section">%s</p>',
-		esc_html('Add your website social media links.','rb-site-social-links')
-    );
+if ( ! function_exists( 'rb_site_social_links_input' ) ) {
+	/**
+	 * Render input field for social links.
+	 *
+	 * @param array $args Input field args.
+	 * @return void
+	 */
+	function rb_site_social_links_input( $args ) {
+		$value = get_option( $args['label_for'] );
+
+		printf(
+			'<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="%4$s" />',
+			esc_attr( $args['label_for'] ),
+			esc_attr( $args['label_for'] ),
+			esc_url( $value ),
+			esc_attr( $args['placeholder'] )
+		);
+	}
 }
 
-// Facebook Link Callback Function Add Here
-function rb_facebook_link_input() {
-    $rb_site_social_links_option = get_option('rb_facebook_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_facebook_link')); ?> */
-
-    printf(
-		'<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://www.facebook.com/bashir.rased">',
-		'rb_facebook_link',
-		'rb_facebook_link',
-		esc_url($rb_site_social_links_option)
-    );
+/**
+ * Enqueue RB icon font files.
+ */
+function rb_site_social_links_font_enqueue() {
+	wp_enqueue_style(
+		'rb-site-social-links-icons',
+		plugin_dir_url( __FILE__ ) . 'assets/style.css',
+		array(),
+		'1.0.0'
+	);
 }
+add_action( 'wp_enqueue_scripts', 'rb_site_social_links_font_enqueue' );
 
-// Twitter Link Callback Function Add Here
-function rb_twitter_link_input() {
-    $rb_site_social_links_option = get_option('rb_twitter_link');
+/**
+ * Output social icons list.
+ *
+ * @param string $ul_id     Optional UL ID.
+ * @param string $ul_class  Optional UL class.
+ * @param string $li_class  Optional LI class.
+ * @param string $a_class   Optional link class.
+ */
+function rb_site_social_links_icon( $ul_id = '', $ul_class = '', $li_class = '', $a_class = '' ) {
 
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_twitter_link')); ?> */
+	// Social link option keys + matching icon classes.
+	$social_icons = array(
+		'rb_twitter_link'   => 'rb-twitter-x',
+		'rb_facebook_link'  => 'rb-facebook',
+		'rb_instagram_link' => 'rb-instagram',
+		'rb_pinterest_link' => 'rb-pinterest',
+		'rb_linkedin_link'  => 'rb-linkedin',
+		'rb_youtube_link'   => 'rb-youtube',
+		'rb_behance_link'   => 'rb-behance',
+		'rb_dribbble_link'  => 'rb-dribbble',
+		'rb_github_link'    => 'rb-github',
+		'rb_whatsapp_link'  => 'rb-whatsapp',
+		'rb_tiktok_link'    => 'rb-tiktok',
+	);
 
-    printf(
-        '<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://twitter.com/BashirRased">',
-        'rb_twitter_link',
-        'rb_twitter_link',
-        esc_url($rb_site_social_links_option)
-    );
+	// Filter only fields with values.
+	$active_links = array_filter(
+		$social_icons,
+		function ( $icon, $option ) {
+			return ! empty( get_option( $option ) );
+		},
+		ARRAY_FILTER_USE_BOTH
+	);
+
+	// Nothing to output.
+	if ( empty( $active_links ) ) {
+		return;
+	}
+
+	// Start UL.
+	echo '<ul'
+		. ( $ul_id ? ' id="' . esc_attr( $ul_id ) . '"' : '' )
+		. ( $ul_class ? ' class="' . esc_attr( $ul_class ) . '"' : '' )
+		. '>';
+
+	// Loop through each active social link.
+	foreach ( $active_links as $option => $icon_class ) {
+
+		$url = get_option( $option );
+
+		echo '<li' . ( $li_class ? ' class="' . esc_attr( $li_class ) . '"' : '' ) . '>';
+
+		echo '<a href="' . esc_url( $url ) . '" target="_blank"'
+			. ( $a_class ? ' class="' . esc_attr( $a_class ) . '"' : '' )
+			. '>';
+
+		echo '<i class="rb-icons ' . esc_attr( $icon_class ) . '"></i>';
+
+		echo '</a>';
+
+		echo '</li>';
+	}
+
+	echo '</ul>';
 }
-
-// Instagram Link Callback Function Add Here
-function rb_instagram_link_input() {
-    $rb_site_social_links_option = get_option('rb_instagram_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_instagram_link')); ?> */
-
-    printf(
-		'<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://www.instagram.com/md.rashedmollik/">',
-        'rb_instagram_link',
-        'rb_instagram_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-// LinkedIn Link Callback Function Add Here
-function rb_linkedin_link_input() {
-    $rb_site_social_links_option = get_option('rb_linkedin_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_linkedin_link')); ?> */
-
-    printf(
-		'<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://www.linkedin.com/in/bashir-rased/">',
-        'rb_linkedin_link',
-        'rb_linkedin_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-// Pinterest Link Callback Function Add Here
-function rb_pinterest_link_input() {
-    $rb_site_social_links_option = get_option('rb_pinterest_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_pinterest_link')); ?> */
-
-    printf(
-        '<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://www.pinterest.com/bashirrased2/">',
-        'rb_pinterest_link',
-        'rb_pinterest_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-// YouTube Link Callback Function Add Here
-function rb_youtube_link_input() {
-    $rb_site_social_links_option = get_option('rb_youtube_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_youtube_link')); ?> */
-
-    printf(
-        '<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://www.youtube.com/channel/UCkaJEqsvmD4gag6gZJQgMkg">',
-        'rb_youtube_link',
-        'rb_youtube_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-// Behance Link Callback Function Add Here
-function rb_behance_link_input() {
-    $rb_site_social_links_option = get_option('rb_behance_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_behance_link')); ?> */
-
-    printf(
-        '<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://www.behance.net/bashirrased2021">',
-        'rb_behance_link',
-        'rb_behance_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-// Dribbble Link Callback Function Add Here
-function rb_dribbble_link_input() {
-    $rb_site_social_links_option = get_option('rb_dribbble_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_dribbble_link')); ?> */
-
-    printf(
-        '<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://www.dribbble.com/bashirrased2/">',
-        'rb_dribbble_link',
-        'rb_dribbble_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-// Github Link Callback Function Add Here
-function rb_github_link_input() {
-    $rb_site_social_links_option = get_option('rb_github_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_github_link')); ?> */
-
-    printf(
-        '<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://github.com/BashirRased">',
-        'rb_github_link',
-        'rb_github_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-// CodePen Link Callback Function Add Here
-function rb_codepen_link_input() {
-    $rb_site_social_links_option = get_option('rb_codepen_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_codepen_link')); ?> */  
-
-    printf(
-        '<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://codepen.io/bashir-rased">',
-        'rb_codepen_link',
-        'rb_codepen_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-// WordPress Link Callback Function Add Here
-function rb_wordpress_link_input() {
-    $rb_site_social_links_option = get_option('rb_wordpress_link');
-
-    /* This code use for frontend page
-    <?php echo esc_url(get_option('rb_wordpress_link')); ?> */ 
-
-    printf(
-        '<input type="url" id="%1$s" class="large-text code" name="%2$s" value="%3$s" placeholder="https://profiles.wordpress.org/bashirrased2017/">',
-        'rb_wordpress_link',
-        'rb_wordpress_link',
-        esc_url($rb_site_social_links_option)
-    );
-}
-
-add_action('admin_init','rb_site_social_links');
--- readme.txt
+++ readme.txt
@@ -2,83 +2,110 @@
 Contributors: bashirrased2017
 Donate link: https://bashirrased.com/
 Requires at least: 6.4
-Tested up to: 6.8
+Tested up to: 6.7
 Requires PHP: 7.0
-Stable tag: 1.0.6
+Stable tag: 1.0.7
 Tags: social links, site links, social media links
 License: GPLv2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-RB Site Social Links plugin use for your site social links add.
+RB Site Social Links plugin helps you dynamically add and display your website’s social media profile links.
 
 == Description ==
 
-RB Site Social Links plugin use for dynamically add your website social icon link. You need change only your social account link.
+RB Site Social Links allows you to easily add and display social media links on your website.  
+Simply enter your social profile URLs in the settings page, and display them anywhere using the function provided.
+
+### ✅ Supported Social Icons
+1. **Twitter**
+2. **Facebook**
+3. **Instagram**
+4. **LinkedIn**
+5. **Pinterest**
+6. **YouTube**
+7. **TikTok**
+8. **Dribbble**
+9. **Behance**
+10. **WhatsApp**
+11. **GitHub**
+
+### 📌 How to Add the Function in Your Theme
+
+Add this code inside your theme’s **header.php**, **footer.php**, or inside a template file:
+
+	if ( function_exists( 'rb_site_social_links_icon' ) ) {
+		rb_site_social_links_icon(
+			'socia_icon_menu', // ul ID
+			'socia_icon_menu', // ul class
+			'socia_icon_item', // li class
+			'socia_icon_link'  // a class
+		);
+	}
+
+You can customize the class names as needed for styling.
 
 == Installation ==
 
 ### From your WordPress dashboard
 
-= Visit =
-Plugins > Add New
-
-= Search =
-for "RB Site Social Links"
-
-= Install and Activate =
-"RB Site Social Links" from your Plugins page
-
-= Go to =
-Setting -> General Page
-
-= Links add =
-Add your website's social media links
+1. Go to **Plugins → Add New**
+2. Search for **"RB Site Social Links"**
+3. Click **Install**, then **Activate**
+4. Go to **Settings → General**
+5. Add your social media profile links
 
 == Frequently Asked Questions ==
 
-= 1. Need any other plugin requirements? =
-Need a plugin [RB Theme Helpler](https://wordpress.org/plugins/rb-theme-helper/)
+= Does this plugin require another plugin? =
+No. RB Site Social Links works independently. You can display the icons anywhere using the function rb_site_social_links_icon().
 
 == Screenshots ==
 
-1. WordPress Dashboard Admin -> Setting -> General Page.
+1. Settings → General page where social links are added.
 
 == Changelog ==
+
+= 1.0.7 =
+* 28.11.2025 – 8th version submission
+* Removed unused social links
+* PHPCS & WPCS code standards applied
+* Add Bengali translation
+
 = 1.0.6 =
-* 08.04.2025 - 7th version submission
-* Remove some social links
+* 08.04.2025 – 7th version submission
+* Removed some social links
 
 = 1.0.5 =
-* 21.04.2024 - 6th version submission
-* Full editing
-* More social icons add
-* Connect with plugin - RB Theme Helper
+* 21.04.2024 – 6th version submission
+* Full plugin revision
+* More social icons added
+* Connected with RB Theme Helper plugin
 
 = 1.0.4 =
-* 29.03.2022 - 5th version submission
-* readme.txt edit
+* 29.03.2022 – 5th version submission
+* Updated readme.txt
 
 = 1.0.3 =
-* 10.12.2021 - 4th version submission
-* Instagram Link Problem Solve
-* readme.txt edit
+* 10.12.2021 – 4th version submission
+* Fixed Instagram link issue
+* Updated readme.txt
 
 = 1.0.2 =
-* 29.10.2021 - 3rd version submission
-* Plugin URI Add
-* Plugin Description change
-* Add new plugin tabs 1. Installation
+* 29.10.2021 – 3rd version submission
+* Added Plugin URI
+* Updated plugin description
+* Added installation instructions
 
 = 1.0.1 =
-* 22.10.2021 - 2nd submission
-* Add Author URI
-* View html php codes in plugin description tab
-* Add Banner & Icon Images
+* 22.10.2021 – 2nd version submission
+* Added Author URI
+* Added HTML/PHP examples
+* Added banner & icon images
 
 = 1.0.0 =
-* 09.09.2021 - 1st submission
+* 09.09.2021 – First release
 
 == Upgrade Notice ==
 
 = 1.0.5 =
-This version is more upgrade this plugin.
+Major update with code improvements, updated instructions, and expanded social icon list.
